3993e743eba86ab0b49ac632b05e60d4bd592e3a
esmail.ahmadipour <esmaeil.ahmadipour@samim.net>
2023-10-27 16:45:31 +0330

    update(app): init project

---
+++ android/app/src/main/kotlin/dev/ea2/flutter_prohibition_use_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.ea2.flutter_prohibition_use_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_prohibition_use_app/app.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
